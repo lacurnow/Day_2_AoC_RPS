a96d26ee16ea51e55eba94bd353bf2d13840eb9c
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-12-03 11:28:37 +0000

    Solution to part 2

--- src/main/scala/ElfRockPaperScissors.scala
+++ src/main/scala/ElfRockPaperScissors.scala
@@ -1,6 +1,7 @@
 import scala.io.Source
 
 case class playerRoundScores(opponentGuess: Int, myElfGuess: Int)
+case class guessAndRoundScore(guessScore: Int, roundScore: Int)
 
 object ElfRockPaperScissors {
   def openFile(filename: String): String = {
@@ -15,7 +16,9 @@ object ElfRockPaperScissors {
     allRounds.map((round: String) => {
       val opponentGuess= round.charAt(0).toString
       val myElfGuess= round.charAt(2).toString
-      generateRoundScores(opponentGuess, myElfGuess)
+//      generateRoundScores(opponentGuess, myElfGuess)
+      val round_part2 = partTwoScoreFinder(opponentGuess, myElfGuess)
+      round_part2.roundScore + round_part2.guessScore
       }
     ).sum
   }
@@ -39,6 +42,26 @@ object ElfRockPaperScissors {
       case ("A", "X") | ("B", "Y") | ("C", "Z") => playerRoundScores(3, 3)
       case ("A", "Y") | ("B", "Z") | ("C", "X") => playerRoundScores(0, 6)
       case ("A", "Z") | ("B", "X") | ("C", "Y") => playerRoundScores(6, 0)
+    }
+  }
+
+  def partTwoScoreFinder(opponentGuess: String, myElfGuess: String): guessAndRoundScore = {
+    myElfGuess match {
+      case "X" => opponentGuess match {
+        case "A" => guessAndRoundScore(3, 0)
+        case "B" => guessAndRoundScore(1, 0)
+        case "C" => guessAndRoundScore(2, 0)
+      }
+      case "Y" => opponentGuess match {
+        case "A" => guessAndRoundScore(1, 3)
+        case "B" => guessAndRoundScore(2, 3)
+        case "C" => guessAndRoundScore(3, 3)
+      }
+      case "Z" => opponentGuess match {
+        case "A" => guessAndRoundScore(2, 6)
+        case "B" => guessAndRoundScore(3, 6)
+        case "C" => guessAndRoundScore(1, 6)
+      }
     }
   }
 }
